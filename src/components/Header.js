c848e7de38714b7fbcfe93d6211fa7abc08f8399
akshay <akshay@opsfuse.com>
2020-04-25 16:52:43 +0530

    added css to component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../style.css';
+
+function Header() {
+    return(<div>
+        <header className="navbar">
+            This is header.
+        </header>
+    </div>)
+}
+
+export default Header
