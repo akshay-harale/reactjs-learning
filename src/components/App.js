0dc75a2dd1d803a7eea5889c0eab55384c5e0808
akshay <akshay@opsfuse.com>
2020-04-27 17:18:32 +0530

    understanding jsx and js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,21 @@ import MainContent from "./MainContent"
 import Footer from "./Footer"
 
 function App() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    if(hours < 12){
+        timeOfDay = "morning!"
+    } else if(hours >=12 && hours <=15) {
+        timeOfDay = "afternoon!"
+    } else {
+        timeOfDay = "night!"
+    }
     return(<div>
-        <Navbar />
+        {/* <Navbar />
         <MainContent />
-        <Footer />
+        <Footer /> */}
+        <h1> Good {timeOfDay}</h1>
     </div>)
 }
 
