72bc093f348584ce15a813766d05e9832129c4d5
akshay <akshay@opsfuse.com>
2020-04-27 19:31:35 +0530

    understanding props in react

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import MainContent from "./MainContent"
 import Footer from "./Footer"
 import TodoItem from "./TodoItem"
 import '../style.css';
+import ContactCard from "./ContactCard"
 
 function App() {
     const date = new Date(2018, 6, 31, 15)
@@ -28,13 +29,30 @@ function App() {
         {/* <Navbar />
         <MainContent />
         <Footer /> */}
-        <h1 style={styles}> Good {timeOfDay}</h1>
+        {/* <h1 style={styles}> Good {timeOfDay}</h1>
         <div className="todo-list">
             <TodoItem />
             <TodoItem />
             <TodoItem />
+        </div> */}
+        <div className="contacts">
+            <ContactCard 
+                contact={{name: "Mr. Whiskerson" ,imgUrl:"http://placekitten.com/300/200" ,phone:"(212) 555-1234" , email:"mr.whiskaz@catnap.meow"}}  
+            />
+            
+            <ContactCard 
+                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+            
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
         </div>
-        
+
     </div>)
 }
 
