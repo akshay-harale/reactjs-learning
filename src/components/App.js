6b6f410183937ca118aca86bc23bf7e19672c645
akshay <akshay@opsfuse.com>
2020-04-27 18:30:19 +0530

    better styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,29 @@ import MainContent from "./MainContent"
 import Footer from "./Footer"
 
 function App() {
-    const date = new Date()
+    const date = new Date(2018, 6, 31, 15)
     const hours = date.getHours()
     let timeOfDay
+    const styles = { 
+        
+        fontSize: 24
+    }
     if(hours < 12){
         timeOfDay = "morning!"
+        styles.color = "#04756F" 
     } else if(hours >=12 && hours <=15) {
         timeOfDay = "afternoon!"
+        styles.color = "#8914A3"
     } else {
         timeOfDay = "night!"
+        styles.color = "#D90000"
     }
+    
     return(<div>
         {/* <Navbar />
         <MainContent />
         <Footer /> */}
-        <h1> Good {timeOfDay}</h1>
+        <h1 style={styles}> Good {timeOfDay}</h1>
     </div>)
 }
 
