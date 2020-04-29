d9fb28b98bea662998d48ea363a7f639e89e937b
akshay <akshay@opsfuse.com>
2020-04-29 08:15:53 +0530

    converting functions to classes.

--- src/components/App.js
+++ src/components/App.js
@@ -11,52 +11,57 @@ import JoksData from "./JokesData"
 import jokesData from "./JokesData"
 import products from "./vschoolProducts"
 import Product from "./Product"
+import todos from "./todosData"
 
-function App() {
-    const date = new Date(2018, 6, 31, 15)
-    const hours = date.getHours()
-    let timeOfDay
-    const styles = { 
-        
-        fontSize: 24
+class App extends React.Component {
+    render() {
+        const date = new Date(2018, 6, 31, 15)
+        const hours = date.getHours()
+        let timeOfDay
+        const styles = {
+
+            fontSize: 24
+        }
+        if (hours < 12) {
+            timeOfDay = "morning!"
+            styles.color = "#04756F"
+        } else if (hours >= 12 && hours <= 15) {
+            timeOfDay = "afternoon!"
+            styles.color = "#8914A3"
+        } else {
+            timeOfDay = "night!"
+            styles.color = "#D90000"
+        }
+
+        const jokes = jokesData.map(joke => <Joke
+            key={joke.id}
+            question={joke.question}
+            punchLine={joke.punchLine}
+        />)
+
+        const vProducts = products.map(p => <Product
+            key={p.id}
+            product={p}
+        />)
+
+        const todosComponents = todos.map(item => <TodoItem key={item.id} item={item} />)
+
+        return (<div>
+            {/* <Navbar />
+            <MainContent />
+            <Footer /> */}
+            {/* <h1 style={styles}> Good {timeOfDay}</h1>
+            <div className="todo-list">
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div> */}
+            <ContactList />
+            {/* {jokes} */}
+            {/* {vProducts} */}
+            {/* {todosComponents} */}
+
+        </div>)
     }
-    if(hours < 12){
-        timeOfDay = "morning!"
-        styles.color = "#04756F" 
-    } else if(hours >=12 && hours <=15) {
-        timeOfDay = "afternoon!"
-        styles.color = "#8914A3"
-    } else {
-        timeOfDay = "night!"
-        styles.color = "#D90000"
-    }
-
-    const jokes = jokesData.map(joke => <Joke 
-        key={joke.id}
-        question={joke.question} 
-        punchLine={joke.punchLine}
-    />)
-
-    const vProducts = products.map(p => <Product 
-        key={p.id}
-        product={p}
-    />)
-    
-    return(<div>
-        {/* <Navbar />
-        <MainContent />
-        <Footer /> */}
-        {/* <h1 style={styles}> Good {timeOfDay}</h1>
-        <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-        </div> */}
-        {/* <ContactList /> */}
-        {/* {jokes} */}
-        {vProducts}
-
-    </div>)
 }
-
 export default App
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,15 +1,17 @@
 import React from "react"
 import '../style.css';
 
-function ContactCard(props){
-    return (
-        <div className="contact-card">
-            <img src={props.contact.imgUrl}/>
-            <h3>{props.contact.name}</h3>
-            <p>Phone: {props.contact.phone}</p>
-            <p>Email: {props.contact.email}</p>
-        </div>
-    )
+class ContactCard extends React.Component {
+    render() {
+        return (
+            <div className="contact-card">
+                <img src={this.props.contact.imgUrl}/>
+                <h3>{this.props.contact.name}</h3>
+                <p>Phone: {this.props.contact.phone}</p>
+                <p>Email: {this.props.contact.email}</p>
+            </div>
+        )
+    }
 }
 
 export default ContactCard
